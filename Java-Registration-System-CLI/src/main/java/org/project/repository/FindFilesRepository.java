6a56fe94f65d47b8fb7a4e5b463ac92050aa00fd
/Maycon Vyctor <mayconsantosfc11@gmail.com>
2026-08-13 23:57:40 -0300

    feat: add file discovery repository

--- Java-Registration-System-CLI/src/main/java/org/project/repository/FindFilesRepository.java
+++ Java-Registration-System-CLI/src/main/java/org/project/repository/FindFilesRepository.java
@@ -1,7 +1,23 @@
 package org.project.repository;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class FindFilesRepository {
-    public List<String> findFiles()
+    public List<String> findFiles(){
+        List<String> fileNames = new ArrayList<>();
+
+        Path path = Paths.get("src/data/users");
+        try (Stream<Path> fluxo = Files.list(path)) {
+            fluxo.forEach(arquivo -> System.out.println(arquivo.getFileName()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileNames;
+    }
 }
